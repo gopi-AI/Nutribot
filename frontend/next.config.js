95a8df4822df5748d6d3ec7eda2a01e7d5f27221
Gopal <60707591+gopi-AI@users.noreply.github.com>
2025-08-22 16:08:10 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://localhost:8000/:path*" // Local FastAPI during dev
-            : "https://my-nutrition-bot-backend.onrender.com/:path*", // Deployed backend
-      },
-    ];
-  },
+  output: 'export', // ensures next export works
 };
 
 module.exports = nextConfig;
-
