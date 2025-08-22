c4557553258b9624140049a73ea1c1f533f0be82
Gopal <60707591+gopi-AI@users.noreply.github.com>
2025-08-22 16:34:25 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,6 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // enable static export
+  output: 'export',
 };
 
 module.exports = nextConfig;
